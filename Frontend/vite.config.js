dbd50f5b0932f6868ccb8990e021c96f34c8ef16
MANNE UDAY KIRAN <143863214+ManneUdayKiran@users.noreply.github.com>
2025-06-24 22:34:17 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['https://speech-to-text-tool.onrender.com','https://speech-to-text-tool-backend.onrender.com']
+
+  }
+});
